c399631398300d777dfe01f1beb01098a3c6945b
Rohan Khode <rohankhode18@gmail.com>
2025-07-23 14:25:42 +0000

    new data added in Variables V3

--- variables.sh
+++ variables.sh
@@ -4,4 +4,5 @@ fname="Mike"
 lname="Cato"
 age=23
 friends=( "Mike" "Sam" "Duke")
-echo "$fname $lname is now $age and has friends ${friends[*]}"
+is_dead=false
+echo "$fname $lname is now $age and has friends ${friends[*]} is dead $is_dead"
