e2789706d576aec83727df5cd72ed53b1ae4bf33
Rohan Khode <rohankhode18@gmail.com>
2025-07-24 11:59:59 +0000

    New User added V4

--- variables.sh
+++ variables.sh
@@ -6,3 +6,10 @@ age=23
 friends=( "Mike" "Sam" "Duke")
 is_dead=false
 echo "$fname $lname is now $age and has friends ${friends[*]} is dead $is_dead"
+
+fname="Samuel"
+lname="Dex"
+age=45
+friends=( "Tora" "EMT" "Dexter")
+is_dead=true
+echo "$fname $lname is now $age and has friends ${friends[*]} is dead $is_dead"
