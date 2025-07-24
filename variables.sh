db60b0f4c8812acbd9455114397fc75ecffd31eb
Rohan Khode <rohankhode18@gmail.com>
2025-07-24 18:38:37 +0000

    Final change for variables.sh

--- variables.sh
+++ variables.sh
@@ -8,3 +8,14 @@ echo "$fname $lname is now $age"
 # Command Substitution (Runs command in subshell)
 todays_date=$(date)
 echo "$todays_date"
+
+# Constants variable (cannot be changed)
+readonly username="delta"
+echo "$username"
+
+# examples
+name="Samuel"
+age=32
+favorite_language="python"
+summary="chilling"
+echo "$name favorite language is $favorite_language and current age is $age in short he is $summary"
