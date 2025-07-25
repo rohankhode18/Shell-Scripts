dc518c5217e324bc6abd41fc361276565c11da3a
Rohan Khode <rohankhode18@gmail.com>
2025-07-25 04:55:25 +0000

    variables changed in dev

--- variables.sh
+++ variables.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-fname="Mike"
-lname="Cato"
-age=23
-friends=( "Mike" "Sam" "Duke")
-is_dead=false
-echo "$fname $lname is now $age and has friends ${friends[*]} is dead $is_dead"
+# variables
+name="dexter"
+is_adult=true
 
-fname="Samuel"
-lname="Dex"
-age=45
-friends=( "Tora" "EMT" "Dexter")
-is_dead=true
-echo "$fname $lname is now $age and has friends ${friends[*]} is dead $is_dead"
+echo "$name and is adult $is_adult"
